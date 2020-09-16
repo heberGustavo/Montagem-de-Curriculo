81b2fad38588e1d5c6708f6458d686e1159970ac
Heber Gustavo <heber.gbarbosa@hotmail.com>
2020-09-16 20:29:15 -0300

    Criação de todas as Models do sistema

--- Montagem de Curriculo/Models/Contexto.cs
+++ Montagem de Curriculo/Models/Contexto.cs
@@ -8,6 +8,13 @@ namespace Montagem_de_Curriculo.Models
 {
     public class Contexto : DbContext
     {
+        public DbSet<Curriculo> Curriculos { get; set; }
+        public DbSet<Idioma> Idiomas { get; set; }
+        public DbSet<TipoCurso> TipoCursos { get; set; }
+        public DbSet<FormacaoAcademica> FormacaoAcademicas { get; set; }
+        public DbSet<Objetivo> Objetivos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
         public Contexto(DbContextOptions<Contexto> options) : base(options)
         {
 
---
+++ Montagem de Curriculo/Models/TipoCurso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Montagem_de_Curriculo.Models
+{
+    public class TipoCurso
+    {
+        public int TipoCursoId { get; set; }
+        public string Tipo { get; set; }
+        public ICollection<FormacaoAcademica> FormacaoAcademicas { get; set; } //Pode estar relacionado a varias Formações Academicas
+    }
+}
