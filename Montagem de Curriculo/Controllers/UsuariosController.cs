ca1d794d4645828dad7446832375766b89619452
Heber Gustavo <heber.gbarbosa@hotmail.com>
2020-09-18 10:42:20 -0300

    Login de Usuários

--- Montagem de Curriculo/Controllers/UsuariosController.cs
+++ Montagem de Curriculo/Controllers/UsuariosController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Montagem_de_Curriculo.Models;
+using Montagem_de_Curriculo.ViewModels;
 
 namespace Montagem_de_Curriculo.Controllers
 {
@@ -33,7 +34,6 @@ namespace Montagem_de_Curriculo.Controllers
             if (ModelState.IsValid)
             {
                 _context.Add(usuario);
-                await _context.SaveChangesAsync();
 
                 //Armazena as Informações de Login
                 InformacaoLogin informacao = new InformacaoLogin
@@ -43,6 +43,9 @@ namespace Montagem_de_Curriculo.Controllers
                     Data = DateTime.Now.ToShortDateString(),
                     Horario = DateTime.Now.ToShortTimeString()
                 };
+
+                _context.Add(informacao);
+                await _context.SaveChangesAsync();
 
                 //Armazena o ID do usuario em uma sessao para usar mais tarde
                 HttpContext.Session.SetInt32("UsuarioId", usuario.UsuarioId);
@@ -63,6 +66,62 @@ namespace Montagem_de_Curriculo.Controllers
             }
             return View(usuario);
         }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            /* Se o USUARIO estiver LOGAR corre o risco de adicionar algo errado na sessão dele,
+             por isso é bom limpar todas as sessão ANTES de LOGAR */
+            if (User.Identity.IsAuthenticated)
+                HttpContext.Session.Clear();
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_context.Usuarios.Any(u => u.Email == login.Email && u.Senha == login.Senha))
+                {
+                    /*Para gravar nas Informações de Log
+                     - Grava o ID do usuario cadastrado
+                     */
+                    var id = _context.Usuarios.Where(u => u.Email == login.Email && u.Senha == login.Senha)
+                        .Select(u => u.UsuarioId).Single();
+
+                    InformacaoLogin informacao = new InformacaoLogin
+                    {
+                        UsuarioId = id,
+                        EnderecoIP = Request.HttpContext.Connection.RemoteIpAddress.ToString(),
+                        Data = DateTime.Now.ToShortDateString(),
+                        Horario = DateTime.Now.ToShortTimeString()
+                    };
+
+                    _context.Add(informacao);
+                    await _context.SaveChangesAsync();
+
+                    //Realizar Login
+                    HttpContext.Session.SetInt32("UsuarioId", id);
+
+                    var claims = new List<Claim> {
+                        new Claim(ClaimTypes.Email, login.Email)
+                    };
+
+                    //Cria conexão do Usuario
+                    var userIdentity = new ClaimsIdentity(claims, "login");
+                    ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                    await HttpContext.SignInAsync(principal);
+
+                    return RedirectToAction("Index", "Curriculos");
+                }
+            }
+
+            return View(login);
+        }
+
+
 
         private bool UsuarioExists(int id)
         {
